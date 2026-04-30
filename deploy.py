5c846950cfe95888fea748985d709c8a1ed25140
Baek003 <155635815+Baek003@users.noreply.github.com>
2026-04-30 13:12:26 +0000

    pulito

--- deploy.py
+++ deploy.py
@@ -6,7 +6,7 @@ api = HfApi()
 # Carica i file
 api.upload_folder(
     folder_path=".",
-    repo_id="Baek003/Progetto_reputazione_di_una_azienda",
+    repo_id="Baek03/Progetto_reputazione_di_una_azienda",
     repo_type="space",
     token=os.environ["HF_TOKEN"],
     ignore_patterns=["*.pyc", "__pycache__", ".git", ".github"]
