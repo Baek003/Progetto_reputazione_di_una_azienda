421caf602f99da2603aec19b257032db450131ac
Baek003 <155635815+Baek003@users.noreply.github.com>
2026-04-30 13:06:22 +0000

    deploy aggiornato

--- deploy.py
+++ deploy.py
@@ -3,15 +3,6 @@ from huggingface_hub import HfApi
 
 api = HfApi()
 
-# Crea lo Space se non esiste
-api.create_repo(
-    repo_id="Baek003/Progetto_reputazione_di_una_azienda",
-    repo_type="space",
-    space_sdk="gradio",
-    exist_ok=True,
-    token=os.environ["HF_TOKEN"]
-)
-
 # Carica i file
 api.upload_folder(
     folder_path=".",
